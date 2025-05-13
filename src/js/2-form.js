d30178015cf50a82c80ae662b0fdba4f922d268b
Viktor Medvid <darkissdark@gmail.com>
2025-05-14 00:05:51 +0200

    Remove unused partials and update styles for new gallery and form pages

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const formData = { email: '', message: '' };
+const form = document.querySelector('.feedback-form');
+const submitButton = document.querySelector('button[type="submit"]');
+
+onPageLoad();
+
+function onPageLoad() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    const { email, message } = parsedData;
+    form.email.value = email;
+    form.message.value = message;
+  }
+}
+
+form.addEventListener('input', onFormInput);
+submitButton.addEventListener('click', onFormSubmit);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { email, message } = formData;
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+  onSubmitSuccess();
+}
+
+function onSubmitSuccess() {
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  for (let key in formData) formData[key] = '';
+  form.reset();
+}
